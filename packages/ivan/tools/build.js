a956c69ffdab25d579ed3e90bf6117a258110719
Gustav Burchardt <gustavburchardt@gmail.com>
2018-12-25 13:40:33 +0100

    Add build scripts

---
+++ packages/ivan/tools/build.js
@@ -0,0 +1,18 @@
+const fs = require('fs-extra')
+const { spawn } = require('child_process')
+
+fs.emptyDirSync('lib')
+
+const ls = spawn('babel', ['src', '--out-dir', 'lib'])
+
+ls.stdout.on('data', (data) => {
+  console.log(`stdout: ${data}`)
+})
+
+ls.stderr.on('data', (data) => {
+  console.log(`stderr: ${data}`)
+})
+
+ls.on('close', (code) => {
+  console.log(`child process exited with code ${code}`)
+})
